1ae34b1fec17cb99f6bf6d6ca90b0c234ce11d60
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2020-11-15 16:13:50 +0400

    Added JQuery Files

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+// menu
+$(".menu-btn").click(function(){
+    $(this).toggleClass("fa-times");
+    $(".top-nav").toggleClass("active")
+});
+
+// skill
+$(".skill-per").each(function(){
+    var $this = $(this);
+    var per = $this.attr("per");
+    $this.css("width", per + "%");
+    $({animatedValue: 0}).animate({animatedValue: per}, {
+        duration: 1000,
+        step: function(){
+            $this.attr("per", Math.floor(this.animatedValue) + "%");
+        },
+        complete: function(){
+            $this.attr("per", Math.floor(this.animatedValue) + "%");
+        }
+    });
+});
